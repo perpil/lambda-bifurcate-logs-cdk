a7ebfcdb2884bdf0e8ee93a592a61f92517e3fa1
perpil <97474956+perpil@users.noreply.github.com>
2024-02-27 10:29:06 -0800

    Add request id, comment removed FilterPattern

--- lib/logfilter-stack.ts
+++ lib/logfilter-stack.ts
@@ -1,32 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
-import {RemovalPolicy} from 'aws-cdk-lib';
-import {Construct} from 'constructs';
-import {RustFunction} from 'cargo-lambda-cdk'
-import {FilterPattern, LogGroup, LogGroupClass, RetentionDays, SubscriptionFilter} from "aws-cdk-lib/aws-logs";
-import {LambdaDestination} from "aws-cdk-lib/aws-logs-destinations";
-import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
-import {Alias, Architecture, FunctionUrlAuthType, LogFormat, Runtime, SystemLogLevel} from "aws-cdk-lib/aws-lambda";
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { RustFunction } from 'cargo-lambda-cdk'
+import { FilterPattern, LogGroup, LogGroupClass, RetentionDays, SubscriptionFilter } from "aws-cdk-lib/aws-logs";
+import { LambdaDestination } from "aws-cdk-lib/aws-logs-destinations";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Alias, Architecture, FunctionUrlAuthType, LogFormat, Runtime, SystemLogLevel } from "aws-cdk-lib/aws-lambda";
 
 export class LogFilterCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const serviceName = "airline-service";
 
-    const logGroup =  new LogGroup(this, 'serviceLogGroup', {
+    const logGroup = new LogGroup(this, 'serviceLogGroup', {
       logGroupName: `/aws/lambda/${serviceName}`,
       logGroupClass: LogGroupClass.STANDARD,
       retention: RetentionDays.THREE_DAYS,
       removalPolicy: RemovalPolicy.DESTROY
     });
 
-    const requestLogGroup =  new LogGroup(this, 'serviceFilteredLogGroup', {
+    const requestLogGroup = new LogGroup(this, 'serviceFilteredLogGroup', {
       logGroupName: `/aws/lambda/${serviceName}-requestlogs`,
       logGroupClass: LogGroupClass.STANDARD,
       retention: RetentionDays.INFINITE,
       removalPolicy: RemovalPolicy.DESTROY
     });
 
-    const logLambda = new NodejsFunction(this,  "service", {
+    const logLambda = new NodejsFunction(this, "service", {
       entry: 'src/handler.ts',
       functionName: serviceName,
       logGroup,
@@ -41,7 +41,7 @@ export class LogFilterCdkStack extends cdk.Stack {
       version: logLambda.currentVersion,
     });
 
-    const filterLambda = new RustFunction(this,  "serviceLogFilter", {
+    const filterLambda = new RustFunction(this, "serviceLogFilter", {
       functionName: `${serviceName}-requestlogs`,
       logGroup: requestLogGroup,
       memorySize: 128,
@@ -51,15 +51,16 @@ export class LogFilterCdkStack extends cdk.Stack {
     });
 
     new SubscriptionFilter(
-        this,
-        'SubscriptionFilter',
-        {
-          logGroup,
-          filterName: 'requestLogsFilter',
-          destination: new LambdaDestination(filterLambda),
-          //filterPattern: FilterPattern.exists('$._aws'),
-          filterPattern: FilterPattern.literal('"_aws"'),
-        }
+      this,
+      'SubscriptionFilter',
+      {
+        logGroup,
+        filterName: 'requestLogsFilter',
+        destination: new LambdaDestination(filterLambda),
+        // wanted to use this filter pattern but it doesn't work
+        // filterPattern: FilterPattern.exists('$._aws'),
+        filterPattern: FilterPattern.literal('"_aws"'),
+      }
     );
 
     const fnUrl = alias.addFunctionUrl({
