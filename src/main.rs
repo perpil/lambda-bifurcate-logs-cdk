92890203f5ab63f20f6d69af8b35be6c976e1ddd
perpil <97474956+perpil@users.noreply.github.com>
2024-02-26 18:53:33 -0800

    Formatting and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use aws_lambda_events::event::cloudwatch_logs::LogsEvent;
+use chrono::NaiveDateTime;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use chrono::{NaiveDateTime};
-use serde_json::{Value};
+use serde_json::Value;
 
 async fn function_handler(event: LambdaEvent<LogsEvent>) -> Result<(), Error> {
-    //Write to the log
+    //For each log event
     for record in event.payload.aws_logs.data.log_events {
+        // Parse as JSON
         let mut json: Value = serde_json::from_str(&record.message).unwrap();
-
-        // Access the _aws object
-        let aws_object = json.as_object_mut().unwrap().get_mut("_aws").unwrap().as_object_mut().unwrap();
-        // Remove the CloudWatchMetrics key
-        aws_object.remove("CloudWatchMetrics");
-
-        println!("{} {}",
-                 NaiveDateTime::from_timestamp_millis(record.timestamp).unwrap().format("%Y-%m-%dT%H:%M:%S%.3fZ").to_string()
-                 , json);
+        // Rip out _aws.CloudWatchMetrics, we don't want to publish metrics again
+        json.as_object_mut()
+            .unwrap()
+            .get_mut("_aws")
+            .unwrap()
+            .as_object_mut()
+            .unwrap()
+            .remove("CloudWatchMetrics");
+        // Print log entry to stdout prefixed by original timestamp in ISO8601 format
+        println!(
+            "{} {}",
+            NaiveDateTime::from_timestamp_millis(record.timestamp)
+                .unwrap()
+                .format("%Y-%m-%dT%H:%M:%S%.3fZ")
+                .to_string(),
+            json
+        );
     }
     Ok(())
 }
